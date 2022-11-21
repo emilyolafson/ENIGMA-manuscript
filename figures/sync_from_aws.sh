a1df562857feb3fcb09b511c3ba1402c9c55f769
Emily Olafson <emily.olafson@mail.mcgill.ca>
2022-11-21 15:10:52 -0500

    ensemble

--- figures/sync_from_aws.sh
+++ figures/sync_from_aws.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-awspath=$1
+awspath=ec2-3-238-206-219.compute-1.amazonaws.com
 
 scp -i ~/eoaws.pem "$awspath":/home/ubuntu/enigma/results/analysis_1/boxplots.png boxplots_1.png
 scp -i ~/eoaws.pem "$awspath":/home/ubuntu/enigma/results/analysis_2/boxplots.png boxplots_2.png
 scp -i ~/eoaws.pem "$awspath":/home/ubuntu/enigma/results/analysis_3/boxplots.png boxplots_3.png
+scp -i ~/eoaws.pem "$awspath":/home/ubuntu/enigma/results/analysis_5/boxplots.png boxplots_5.png
